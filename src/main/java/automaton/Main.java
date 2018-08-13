51b04e8f558c1e283386e3b34456b047acff1179
epord <pedro.balaguer@hotmail.com>
2018-08-12 22:09:12 -0300

    Added parameter arguments

--- src/main/java/automaton/Main.java
+++ src/main/java/automaton/Main.java
@@ -2,11 +2,29 @@ package automaton;
 
 import java.io.*;
 
+import static java.lang.System.exit;
+
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Integer iterations = 50;
-        Integer rule = 5766;
+        if (args.length < 1) {
+            System.out.println("Filename required");
+            exit(1);
+        }
+        String filename = args[0];
+
+
+        if (args.length < 2) {
+            System.out.println("Iteration count required");
+            exit(1);
+        }
+        Integer iterations = Integer.parseInt(args[1]);
+
+
+        Integer rule = 2333;
+        if (args.length >= 3) {
+            rule = Integer.parseInt(args[1]);
+        }
 
 //        World world = new World(4,5, 1);
 //        world.setCellState(1,0, 0, State.ALIVE);
@@ -17,7 +35,7 @@ public class Main {
 
         FileManager fm = new FileManager();
 
-        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        BufferedReader br = new BufferedReader(new FileReader(filename));
         World world = fm.readWorld(br);
 
         File fout = new File("p5/empty-example/output.txt");
